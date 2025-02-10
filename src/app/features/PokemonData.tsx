7273a37e622319aad20c3a9f54d46cb794225777
Argel <107836860+MarkMalihan@users.noreply.github.com>
2025-02-10 17:52:08 +0800

    Updates
    added react-query sample
    updated PokemonData.test.tsx

--- src/app/features/PokemonData.tsx
+++ src/app/features/PokemonData.tsx
@@ -1,65 +1,8 @@
-import React, { useState } from "react";
-import { Pokemon } from "@/models/AppModels";
-import { searchPokemon } from "@/services/SampleService";
 import { Button } from "@/components/ui/Button";
+import usePokemon from "@/hooks/usePokemon";
 
 const PokemonData = () => {
-  const [pokemon, setPokemon] = useState<Pokemon | null>(null);
-  const [searchQuery, setSearchQuery] = useState<string>("");
-  const [loading, setLoading] = useState<boolean>(false);
-  const [error, setError] = useState<string | null>(null);
-
-  const fetchPokemon = async (query: string) => {
-    if (!query.trim()) {
-      return;
-    }
-
-    setLoading(true);
-    setError(null);
-    setPokemon(null);
-
-    try {
-      const result = await searchPokemon(query.trim().toLowerCase());
-
-      if (result) {
-        setPokemon(result);
-      } else {
-        setError("No Pokémon found. Please try another name or ID.");
-      }
-    } catch (err) {
-      setError("Failed to fetch Pokémon data. Please try again.");
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  const handleSearch = (e: React.FormEvent) => {
-    e.preventDefault();
-    fetchPokemon(searchQuery);
-  };
-
-  const typeColors: Record<string, string> = {
-    normal: "bg-gray-500",
-    fighting: "bg-red-500",
-    flying: "bg-blue-400",
-    poison: "bg-purple-500",
-    ground: "bg-yellow-500",
-    rock: "bg-gray-700",
-    bug: "bg-green-500",
-    ghost: "bg-indigo-500",
-    steel: "bg-gray-600",
-    fire: "bg-orange-500",
-    water: "bg-blue-500",
-    grass: "bg-green-500",
-    electric: "bg-yellow-400",
-    psychic: "bg-pink-500",
-    ice: "bg-teal-400",
-    dragon: "bg-red-700",
-    dark: "bg-gray-800",
-    fairy: "bg-pink-300",
-    unknown: "bg-gray-400",
-    shadow: "bg-black", // Shadow is special, can be handled as such
-  };
+  const {handleSearch, searchQuery, setSearchQuery, isLoading, error, pokemon, typeColors} = usePokemon()
 
   return (
     <div className="flex flex-col items-center justify-center p-4">
@@ -84,8 +27,8 @@ const PokemonData = () => {
         </Button>
       </form>
 
-      {/* Loading State */}
-      {loading && (
+      {/* isLoading State */}
+      {isLoading && (
         <div className="text-lg font-semibold text-gray-600 dark:text-gray-300">
           Loading...
         </div>
@@ -93,11 +36,11 @@ const PokemonData = () => {
 
       {/* Error Message */}
       {error && (
-        <div className="text-lg font-semibold text-red-600">{error}</div>
+        <div className="text-lg font-semibold text-red-600"> {error instanceof Error ? error.message : "An error occurred while fetching the data."}</div>
       )}
 
       {/* Pokémon Data */}
-      {pokemon && !loading && (
+      {pokemon && !isLoading && (
         <div className="bg-gray-300 dark:bg-slate-800 rounded-lg p-8 max-w-sm w-full text-center">
           <img
             className="w-32 h-32 mx-auto"
